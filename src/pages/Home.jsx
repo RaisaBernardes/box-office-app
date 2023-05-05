ec8c589677b950b07a4ad2c789dc8a3863ec1488
Raisa Bernardes <raisa.rmro@gmail.com>
2023-05-05 11:00:14 -0300

    rendering api data on interface

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,29 @@
 import { useState } from 'react';
+import { searchForShows } from './../api/tvmaze';
 
 const Home = () => {
   const [searchStr, setSearchStr] = useState('');
+  const [apiData, setApiData] = useState([]);
 
   const onSearchInputChange = event => {
     setSearchStr(event.target.value);
   };
 
-  const onSearch = async (event) => {
+  const onSearch = async event => {
     event.preventDefault();
 
-    const response = await fetch(`https://api.tvmaze.com/search/shows?q=${searchStr}`);
-    const body = await response.json();
-    console.log(body);
+    const result = await searchForShows(searchStr); //data coming from api
+    setApiData(result);
   };
+
+  const renderApiData = () => {
+    if(apiData) {// if apiData has data or is "true"
+      return apiData.map((data) => (
+        <div key={data.show.id}>{data.show.name}</div>
+      ));
+    };
+    return null; //if apiData has no data or is "false" 
+  }
 
   return (
     <div>
@@ -22,6 +32,10 @@ const Home = () => {
         {/*Don't forget to put "value" = two-way data binding*/}
         <button type="submit">Search</button>
       </form>
+
+      <div>
+        {renderApiData()}
+      </div>
     </div>
   );
 };
