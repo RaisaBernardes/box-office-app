9bfe0d62090e50f0abd2959fbdbc011c842c4c7e
Raisa Bernardes <raisa.rmro@gmail.com>
2023-05-12 10:18:14 -0300

    fetching data in Show.jsx with useEffect

--- src/components/shows/ShowCard.jsx
+++ src/components/shows/ShowCard.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 
-const ShowCard = ({ name, image, summary, id }) => {
+const ShowCard = ({ name, image, id, summary }) => {
 
     const summaryStripped = summary 
     ? summary.split(' ').slice(0, 10).join(' ').replace(/<.+?>/g, '')
