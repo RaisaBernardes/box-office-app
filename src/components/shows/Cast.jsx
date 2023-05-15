f5963ab50d99a6307202a547990e33bd4c3834aa
Raisa Bernardes <raisa.rmro@gmail.com>
2023-05-15 19:29:09 -0300

    correcting empty cast array message

--- src/components/shows/Cast.jsx
+++ src/components/shows/Cast.jsx
@@ -1,18 +1,24 @@
 const Cast = ({ cast }) => {
-  return (
-    <div>
-      {cast.map(({ person, character, voice }) => (
-        <div key={person.id}>
-          <div>
-            <img src={person.image ? person.image.medium : '/not-found.png'} />
+
+  if(cast.length === 0){
+    return <div>No cast information available.</div>
+  } else {
+    return (
+      <div>
+        {cast.map(({ person, character, voice }) => (
+          <div key={person.id}>
+            <div>
+              <img src={person.image ? person.image.medium : '/not-found.png'} />
+            </div>
+            <div>
+              {person.name} | {character.name} {!!voice && '| Voiceover'}
+            </div>
           </div>
-          <div>
-            {person.name} | {character.name} {!!voice && '| Voiceover'}
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+        ))}
+      </div>
+    );
+  }
+
 };
 
 export default Cast;
